35e28ec4af81dcf3d9aa0f921e4f8cb51d40f31e
stux <hello@mstdn.social>
2022-07-05 02:23:02 +0200

    Added post delte

--- resources/views/layouts/components/anon-footer.blade.php
+++ resources/views/layouts/components/anon-footer.blade.php
@@ -1,10 +1,11 @@
 <div class="lg:mb-5 py-3 uk-link-reset">
     <div class="flex flex-col items-center justify-between lg:flex-row max-w-6xl mx-auto lg:space-y-0 space-y-3">
         <div class="flex space-x-2 text-gray-700 uppercase">
-            <a href="{{ route('about') }}"> About</a>
-            <a href="{{ route('terms') }}"> Terms</a>
-            <a href="{{ route('privacy') }}"> Privacy</a>
+            <a href="{{ route('about') }}"> About</a> 
+            <a href="{{ route('terms') }}"> Terms</a> 
+            <a href="{{ route('privacy') }}"> Privacy</a> 
+            <a href="{{ route('contact') }}"> Contact</a> 
         </div>
-        <p class="capitalize">© 2022. GOLDFISH</p>
+        <p class="capitalize">© 2022. Goldfish</p>
     </div>
-</div>
+</div>  
