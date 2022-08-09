6449d44e2e7095c2e3392dfe0d1581f035402856
stux <hello@mstdn.social>
2022-08-09 19:13:52 +0200

    Added Horizon & pagination to user profile posts

--- resources/js/Pages/Users/Show.vue
+++ resources/js/Pages/Users/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Jetstream/Welcome.vue';
+import Pagination from '../Shared/Pagination.vue';
 
 let props = defineProps({
   profile: Object,
@@ -121,8 +122,9 @@ let props = defineProps({
                             </div>
                         </div>    
                     </div>
-
+        <Pagination :links="profile.posts.links" />
         </div>
+
 
         </section>
 
