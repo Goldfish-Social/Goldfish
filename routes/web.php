ae3f221cd62e30f4e8fef3e8c72a8df14905c945
stux <hello@mstdn.social>
2022-08-09 21:54:23 +0200

    Card and composer updates

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Models\User;
-use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Request;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TimelineController;
@@ -24,25 +20,5 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
     Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
     Route::get('/public', [TimelineController::class, 'public'])->name('public-timeline');
     Route::delete('/posts/{post}/delete', [PostController::class, 'destroy'])->name('posts.destroy');
-    Route::get('/users', function () {
-        return Inertia::render('Users/Index', [
-            'users' => User::query()
-
-            ->when(Request::input('search'), function ($query, $search) {
-                $query->where('username', 'like', "%{$search}%");
-            })
-            
-            ->paginate(10)
-            ->withQueryString()
-            ->through(fn($user) => [
-                'id'    =>  $user->id,
-                'name'  =>  $user->name,
-                'username'  =>  $user->username,
-                'avatar'    => $user->getProfilePhotoUrlAttribute(),
-                'about'     => $user->about
-            ]),
-
-            'filters' => Request::only(['search'])
-        ]);
-    })->name('users');
+    
 });
