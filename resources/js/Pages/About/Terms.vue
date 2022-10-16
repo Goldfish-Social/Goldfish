28a08b9e924a12df421b0fc7bc0537297e12cb19
stux <hello@mstdn.social>
2022-10-16 23:13:28 +0200

    Remove 'funny' licence part

--- resources/js/Pages/About/Terms.vue
+++ resources/js/Pages/About/Terms.vue
@@ -31,43 +31,7 @@ import { Head } from "@inertiajs/inertia-vue3";
               accessing this site. The materials contained in this website are
               protected by applicable copyright and trademark law.
             </p>
-            <h5>2. Use License</h5>
-            <ol type="a">
-              <li>
-                Permission is granted to temporarily download one copy of the
-                materials (information or software) on Goldfish's website for
-                personal, non-commercial transitory viewing only. This is the
-                grant of a license, not a transfer of title, and under this
-                license you may not:
-                <ol type="i">
-                  <li>modify or copy the materials;</li>
-                  <li>
-                    use the materials for any commercial purpose, or for any
-                    public display (commercial or non-commercial);
-                  </li>
-                  <li>
-                    attempt to decompile or reverse engineer any software
-                    contained on Goldfish's website;
-                  </li>
-                  <li>
-                    remove any copyright or other proprietary notations from the
-                    materials; or
-                  </li>
-                  <li>
-                    transfer the materials to another person or "mirror" the
-                    materials on any other server.
-                  </li>
-                </ol>
-              </li>
-              <li>
-                This license shall automatically terminate if you violate any of
-                these restrictions and may be terminated by Goldfish at any
-                time. Upon terminating your viewing of these materials or upon
-                the termination of this license, you must destroy any downloaded
-                materials in your possession whether in electronic or printed
-                format.
-              </li>
-            </ol>
+            
             <h5>3. Disclaimer</h5>
             <ol type="a">
               <li>
