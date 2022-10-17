f396d6dddbbd13bda19f0432056783c48b894d86
stux <hello@mstdn.social>
2022-10-17 13:41:44 +0200

    Icon algin fix profile page

--- resources/js/Pages/Shared/ProfileCard.vue
+++ resources/js/Pages/Shared/ProfileCard.vue
@@ -53,7 +53,7 @@
                 <p class="my-2 ml-2 text-left">{{ profile.about }}</p>
                 <div class="flex my-4">
                     <div class="flex justify-start">
-                        <div class="hidden sm:block">
+                        <div class="hidden sm:flex">
                             <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 mt-1" width="14" height="14"
                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
                                 stroke-linecap="round" stroke-linejoin="round">
