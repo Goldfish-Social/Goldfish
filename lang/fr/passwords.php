df1c23bdee1836561ff9086e6dcabc848d33c057
LeJun <lejun@gmx.fr>
2023-05-30 08:43:19 +0200

    French localization

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Mot de passe réinitialisé !',
+    'sent' => 'Le lien de réinitialisation vous a été envoyé par mail !',
+    'throttled' => 'Veuillez attendre avant de réessayer.',
+    'token' => 'Clé de réinitialisation incorrecte.',
+    'user' => "Pas de compte associé avec cette adresse mail.",
+
+];
